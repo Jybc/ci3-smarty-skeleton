052f8e0a8afa47bc76c160822e68b0215e1e4eb8
Mu <sparkinzy@163.com>
2020-08-05 16:16:44 +0800

    更换前端构建工具为blendme

--- resources/gulp.sh
+++ resources/gulp.sh
@@ -4,5 +4,5 @@ CURRENT_DIR=$(cd $(dirname $0); pwd)
 MODELES_PACKETE= "/*/*/node_modules/"
 # node_modules 这个完整包固定存储在这个位置，适用于linux centos6.10环境
 rsync -av $MODELES_PACKETE $CURRENT_DIR/node_modules
-gulp
+yarn run blendme build
 echo "gulp successed"
